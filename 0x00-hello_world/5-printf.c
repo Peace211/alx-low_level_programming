5fe5e05a2fd274789b73c6205749cd740383dd83
Peace211 <125648411+Peace211@users.noreply.github.com>
2023-03-20 14:13:20 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
  /**
   * main - This is a main function
   *
-  * Return: when successful
+  * Return: 0 when successful
   */
 
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
