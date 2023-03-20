55574b947b27fe02937bc955b20ab05899d6d8c6
Peace211 <125648411+Peace211@users.noreply.github.com>
2023-03-20 13:59:37 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-  /**
-  4  * main - This is a main function
-  5  *
-  6  * Return: always 0
-  7  */
- 
-  int main(void)
-  {
-          puts("\"Programming is like building a multilingual puzzle");
-          return (0);
-  }
+/**
+ * main - This is a main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
