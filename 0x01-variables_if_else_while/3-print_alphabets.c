46deeeee2c780d31a0ee8f924ff8e82801a2e95b
Peace211 <125648411+Peace211@users.noreply.github.com>
2023-03-20 18:31:10 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char i;
+
+for (i = 'a'; i < 'z'; i++)
+putchar(i);
+
+for (i = 'A'; i < 'Z'; i++)
+putchar(i);
+
+putchar('\n');
+
+return (0);
+}
