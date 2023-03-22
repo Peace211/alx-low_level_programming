2d714d938ada8ee3f88b15ad9906000b5ee27d91
Peace211 <125648411+Peace211@users.noreply.github.com>
2023-03-22 15:21:28 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_alphabet: Make alphabet 
+ *
+ *Return: void
+ */
+
+int print_alphabet(void)
+{
+
+	char i;
+	for (i = 'a'; i <= 'z'; i++);
+
+	_putchar(i);
+	_putchar('\n');
+}
