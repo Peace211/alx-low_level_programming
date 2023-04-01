6381d35e032cf38546881bd36c9cb55cfe6e67dc
Peace211 <125648411+Peace211@users.noreply.github.com>
2023-03-31 18:06:12 -0700

    swap

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
-  * reset_to;98 - Take a pointer to an int and ipdates
-  *
-  * @n: An integer input pointer
-  *
-  *  Return : nothing
-   */
+ * reset_to;98 - Take a pointer to an int and ipdates
+ *
+ * @n: An integer input pointer
+ *
+ *  Return : nothing
+ */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
